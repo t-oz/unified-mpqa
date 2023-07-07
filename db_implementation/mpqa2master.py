553ee4ea62289400984fc26189a8983784f30767
Tyler Osborne <tyler.922@hotmail.com>
2023-07-07 18:55:00 -0400

    working on sentences table

--- db_implementation/mpqa2master.py
+++ db_implementation/mpqa2master.py
@@ -8,6 +8,7 @@ from time import time
 import pprint
 import glob
 import os
+import re
 from progress.bar import Bar
 
 
@@ -19,9 +20,22 @@ class MPQA2MASTER:
         self.con = sqlite3.connect("mpqa_master.db")
         self.cur = self.con.cursor()
 
+        self.master_sentences = []
+        self.master_mentions = []
+        self.master_sources = []
+        self.master_attitudes = []
+
         self.agents = {}
         self.csds = []
         self.targets = {}
+
+        self.csds_expr_subj = []
+        self.encountered_sentences = {}
+
+        self.next_global_sentence_id = 1
+        self.next_global_token_id = 1
+        self.next_global_source_id = 1
+        self.next_global_attitude_id = 1
 
         self.pp = pprint.PrettyPrinter()
 
@@ -61,17 +75,62 @@ class MPQA2MASTER:
 
         print("\nLoading Python data into master schema...")
         self.load_data()
-
-        self.con.close()
-
-
-    def load_data(self):
-        i = annotation_types = set()
-        for annotation in self.csds:
-            annotation_types.add(annotation["annotation_type"])
-        self.pp.pprint(annotation_types)
+        self.exec_sql()
 
         self.con.commit()
+        self.con.close()
+
+    def exec_sql(self):
+        self.con.executemany('INSERT INTO SENTENCES (sentence_id, file, file_sentence_id, sentence)'
+                             'VALUES (?, ?, ?, ?);', self.master_sentences)
+
+    # process a single expressive subjective annotation and its relevant source(s)
+    def proc_expr_subj(self, annotation):
+        pass
+
+    def load_data(self):
+        # i = annotation_types = set()
+        # for annotation in self.csds:
+        #     annotation_types.add(annotation["annotation_type"])
+        # self.pp.pprint(annotation_types)
+        test_sentences = set()
+        # loop over all annotations, executing different functions for each respective annotation type
+        bar = Bar("Annotations Processed", max=len(self.csds))
+        for annotation in self.csds:
+            # all annotation types will require identical processing in many areas
+            file = annotation['doc_id']
+            sentence = annotation['text']
+
+            test_sentences.add(sentence)
+
+            # careful to add only unique sentences to the DB
+            if annotation['sentence_id'] not in self.encountered_sentences:
+                global_sentence_id = self.next_global_sentence_id
+                self.encountered_sentences[annotation['sentence_id']] = self.next_global_sentence_id
+                self.next_global_sentence_id += 1
+                new_sentence_insert = True
+            else:
+                global_sentence_id = self.encountered_sentences[annotation['sentence_id']]
+                new_sentence_insert = False
+
+            # extracting file sentence id
+            file_sentence_id = int(annotation['sentence_id'].split('&&')[1].split('-')[1])
+
+            # if it is a new sentence, prep it for SQL insertion
+            if new_sentence_insert:
+                self.master_sentences.append([global_sentence_id, file, file_sentence_id, sentence])
+
+            # with sentences populated, the code's behavior diverges depending on the annotation type
+            bar.next()
+            continue
+
+            if annotation['annotation_type'] == 'expressive_subjectivity':
+                self.proc_expr_subj(annotation)
+            else:
+                continue
+
+        bar.finish()
+        print(len(test_sentences))
 
 
 if __name__ == "__main__":
